abebe2b681a3647f67722385ae74322de70d76b1
joseBoo7 <jmamanigo@unsa.edu.pe>
2022-10-30 15:50:55 -0500

    Component_Articulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrabajadorComponent } from './trabajador/trabajador.component';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrabajadorComponent
+    TrabajadorComponent,
+    ArticulosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
